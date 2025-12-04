019f303c4900f9d191995fffddc6681d004ef0c6
Rakesh <rakesh.srikanth@knightec.se>
2025-12-04 22:30:24 +0100

    Add Terminal window with controls and integrate GSAP for animations

---
+++ src/components/WindowControls.tsx
@@ -0,0 +1,18 @@
+import useWindowStore from '#store/window'
+import React from 'react'
+
+const WindowControls = ({ target }: { target: string }) => {
+    const { closeWindow } = useWindowStore()
+
+    return (
+        <div id="window-controls" className='mt-2 ml-2' >
+            <div className='close' onClick={() => closeWindow(target)} />
+            <div className='minimize' />
+            <div className='maximize' />
+
+
+        </div>
+    )
+}
+
+export default WindowControls
